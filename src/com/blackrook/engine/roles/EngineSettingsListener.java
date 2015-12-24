3594da46056bba398ce03a05e860e87a39065d1a
mtrop <mtropiano@gmail.com>
2015-12-23 19:48:03 -0500

    Some refactoring.

--- src/com/blackrook/engine/roles/EngineSettingsListener.java
+++ src/com/blackrook/engine/roles/EngineSettingsListener.java
@@ -17,29 +17,29 @@ public interface EngineSettingsListener
 	/**
 	 * Puts this element's user-specific settings into the provided {@link Properties} object.
 	 * Invoked when {@link Engine} has {@link Engine#saveSettings()} called on it.
-	 * @param properties the Properties object to store settings into.
+	 * @param settings the Properties object to store settings into.
 	 */
-	public void onSaveUserSettings(Properties properties);
+	public void onSaveUserSettings(Properties settings);
 	
 	/**
 	 * Puts this element's global settings into the provided {@link Properties} object.
 	 * Invoked when {@link Engine} has {@link Engine#saveSettings()} called on it.
-	 * @param properties the Properties object to store settings into.
+	 * @param settings the Properties object to store settings into.
 	 */
-	public void onSaveGlobalSettings(Properties properties);
+	public void onSaveGlobalSettings(Properties settings);
 	
 	/**
 	 * Sets this element's user-specific settings by reading them from the provided {@link Properties} object.
 	 * Invoked when {@link Engine} has {@link Engine#loadSettings()} called on it.
-	 * @param properties the Properties object to read settings from.
+	 * @param settings the Properties object to read settings from.
 	 */
-	public void onLoadUserSettings(Properties properties);
+	public void onLoadUserSettings(Properties settings);
 	
 	/**
 	 * Sets this element's global settings by reading them from the provided {@link Properties} object.
 	 * Invoked when {@link Engine} has {@link Engine#loadSettings()} called on it.
-	 * @param properties the Properties object to read settings from.
+	 * @param settings the Properties object to read settings from.
 	 */
-	public void onLoadGlobalSettings(Properties properties);
+	public void onLoadGlobalSettings(Properties settings);
 	
 }
